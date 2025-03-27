fc893e1de0cff49c673c57da118e77915cd0d4f6
Hasini Bandara <120240305+HasiniBandara@users.noreply.github.com>
2025-03-27 09:38:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
     let menuIcon = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
 
+    let sections = document.querySelectorAll('section');
+
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+            let navLinks = document.querySelectorAll('header nav a');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*=' + id + ' ]').classList.add
+                        ('active')
+                })
+            }
+        })
+    }
+
     menuIcon.onclick = () => {
         menuIcon.classList.toggle('bx-x');
         navbar.classList.toggle('active');
-    };
+    }
 
     let navLinks = document.querySelectorAll('.navbar a');
 
@@ -54,3 +74,4 @@ document.addEventListener('DOMContentLoaded', function () {
     handleResponsive();
     window.addEventListener('resize', handleResponsive);
 });
+
